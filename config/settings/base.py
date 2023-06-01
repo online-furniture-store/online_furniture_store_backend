8220c9bfa6bcc6db3d48b21aac92b2bc71ecb4b5
saper663 <saper663ru@gmail.com>
2023-06-01 21:52:35 +0500

    build: settings_ci_cd изменяет путь хранения файлов статики

--- config/settings/base.py
+++ config/settings/base.py
@@ -123,7 +123,7 @@ MIDDLEWARE = [
 # STATIC
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = str(BASE_DIR / 'staticfiles')
+STATIC_ROOT = '/var/www/django/static'
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
