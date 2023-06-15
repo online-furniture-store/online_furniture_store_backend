8a0830a9a902fc6317815f2e69b1b966b9029bb5
saper663 <saper663ru@gmail.com>
2023-06-16 00:15:08 +0500

    fix: feat/feedbacks исправляет код согласно кодстайлу

--- apps/users/validators.py
+++ apps/users/validators.py
@@ -4,8 +4,7 @@ from django.core.exceptions import ValidationError
 
 
 def validate_phone(value):
-    if match(r'^((8|'
-             r'\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7}$', value):
+    if match(r'^((8|' r'\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7}$', value):
         return ''.join([i for i in value if i.isdigit()][-10:])
     raise ValidationError('Некорректный номер телефона.')
 
