fe63d00d7663fb3ab8ac868cade4c3f9ad9e8d0c
saper663 <saper663ru@gmail.com>
2023-06-19 17:54:17 +0500

    fix: feat/feedbacks добавляет возможность просматривать отзыв не только автору

--- common/permisions.py
+++ common/permisions.py
@@ -5,4 +5,4 @@ class IsOwner(permissions.BasePermission):
     message = 'Просматривать/редактировать отзыв может только пользователь который его создал'
 
     def has_object_permission(self, request, view, obj):
-        return obj.user == request.user or request.user.is_staff
+        return bool(obj.user == request.user or request.method in permissions.SAFE_METHODS)
