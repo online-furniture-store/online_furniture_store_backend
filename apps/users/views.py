c974566bf946d9e295cac4de75d8b2f7c3f3ec3f
artek22 <ya.geffixe@yandex.ru>
2023-06-26 15:30:34 +0300

    Cleaning garbage

--- apps/users/views.py
+++ apps/users/views.py
@@ -11,3 +11,4 @@ class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericV
     serializer_class = UserSerializer
     queryset = User.objects.all()
     lookup_field = 'pk'
+
